e997379910602064ce993f789db61637c1780e10
2016Alisa <1229266765@qq.com>
2017-07-18 23:39:54 +0800

    自动化文件创建

---
+++ AutomationProject/AutomationProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutomationProject.Startup))]
+namespace AutomationProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
